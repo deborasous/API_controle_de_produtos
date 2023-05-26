84db1ab5e276fefce7253d74be9b67048fa98545
deborasous <deborapatriciadp27@gmail.com>
2023-05-26 20:33:54 -0300

    [M2S03] Ex 3 - Criando o switch para monitorar os métodos

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const http = require("http")
 const fs = require("fs")
 
 const server =http.createServer((request, response)=>{
-  response.statusCode= 200
-  response.setHeader("Content-type", "text/plain")
-  response.end("Funcionando")
+  //roteamento
+  switch (request.method) {
+    case "GET":
+      
+      break;
+    case "POST":
+    
+    break;
+  }
 })
 
 const port =3333
