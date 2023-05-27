7b1ff0db9dd6a8fce67b63445fb0d9a00c6a7f89
deborasous <deborapatriciadp27@gmail.com>
2023-05-26 21:11:11 -0300

    [M2S03] Ex 4 - Criando a função para retornar a lista de produtos

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const http = require("http")
 const fs = require("fs")
 
+function readFileJson(fileJson, callback) {
+  fs.readFileSync(fileJson, "utf-8", (error, data)=>{
+    if (error) {
+      callback(error, null)
+      return
+    }
+
+    try {
+      const productData = JSON.parse(data)
+
+      //verificar se os dados do array é uma lista
+      if (!Array.isArray(productData)) {
+        const error = new Error("Os dados do arquivo não são uma lista.")
+        callback(error, null)
+        return
+      }
+
+      callback(null, productData)
+    } catch (error) {
+      callback(error, null)
+    }
+  })
+}
+
+
 const server =http.createServer((request, response)=>{
+
   //roteamento
   switch (request.method) {
     case "GET":
